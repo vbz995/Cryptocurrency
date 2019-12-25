fc77d14290598c97a6a75c1bdd23a3a95100b192
Bozidar Vrbarac <vrbaracbozidar@gmail.com>
2019-12-25 15:33:31 +0100

    Updates

---
+++ precache-manifest.06d811429dccd8cda91a525ac5d27cd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc66bbc7e107f843b713116bc852353b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4cf0630279f006066ee",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "aa72689620b0d454c12f",
+    "url": "/static/css/main.cf24e7a6.chunk.css"
+  },
+  {
+    "revision": "c4cf0630279f006066ee",
+    "url": "/static/js/2.c4093d3f.chunk.js"
+  },
+  {
+    "revision": "1a3ba1185bf697297fa4b180bfc93f40",
+    "url": "/static/js/2.c4093d3f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa72689620b0d454c12f",
+    "url": "/static/js/main.7a0185cf.chunk.js"
+  },
+  {
+    "revision": "f1c0c5fca436c46e39f3",
+    "url": "/static/js/runtime-main.62bfc51b.js"
+  }
+]);
